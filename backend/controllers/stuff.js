1efaa0a32050ee0fbe9d422b119bda58339bf707
Tristan DAVID <tristandavid50@yahoo.fr>
2021-08-16 10:17:54 +0200

    Ajout multer config

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -3,44 +3,22 @@ const Thing = require('../models/thing')
 // ---- Début CRUD  ----
 
 exports.createThing = (req, res, next) => {
+  const thingObject = JSON.parse(req.body.thing)
+  delete thingObject._id
   const thing = new Thing({
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
-  });
-  thing.save().then(
-    () => {
-    // Création de ressources avec le code 201
-      res.status(201).json({
-        message: 'Post saved successfully!'
-      })
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      })
-    }
-  )
+    ...thingObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  })
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }))
 }
 
 // Récupération de l'id de l'objet grâce à "findOne()" pour trouver le "Thing" ayant le même "_id" que le paramètre de la requête
 exports.getOneThing = (req, res, next) => {
-  Thing.findOne({
-    _id: req.params.id
-  }).then(
-    (thing) => {
-      res.status(200).json(thing);
-    }
-  ).catch(
-    (error) => {
-      res.status(404).json({
-        error: error
-      })
-    }
-  )
+  Thing.findOne({_id: req.params.id})
+  .then((thing) => {res.status(200).json(thing)})
+  .catch((error) => {res.status(404).json({error: error})})
 }
 
 // Mise à jour du "Thing" (premier argument) grâce à "updateOne()" qui permet de modifier nos objets
@@ -54,50 +32,27 @@ exports.modifyThing = (req, res, next) => {
     userId: req.body.userId
   })
   // Utilisation du paramètre "id" remplacé par "Thing" passé comme second argument
-  Thing.updateOne({_id: req.params.id}, thing).then(
-    () => {
+  Thing.updateOne({_id: req.params.id}, thing).then(() => {
       res.status(201).json({
-        message: 'Thing updated successfully!'
-      })
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      })
-    }
-  )
+        message: 'Thing updated successfully!'})
+    })
+    .catch((error) => {res.status(400).json({error: error})
+    })
 }
 
 // On passe un objet à "deleteOne()" correspondant au document à supprimer
 exports.deleteThing = (req, res, next) => {
-  Thing.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
-      })
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      })
-    }
-  )
+  Thing.deleteOne({_id: req.params.id}).then(() => {
+      res.status(200).json({message: 'Deleted!'})
+    })
+    .catch((error) => {res.status(400).json({error: error})})
 }
 
-exports.getAllStuff = (req, res, next) => {
-  Thing.find().then(
-    (things) => {
-      res.status(200).json(things);
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      })
-    }
-  )
+exports.getAllStuff = (req, res, next) => 
+{Thing.find().then((things) => 
+  {res.status(200).json(things)})
+  .catch((error) => {res.status(400).json({error: error})
+    })
 }
 
 // ---- FIN CRUD (Create, Read, Update, Delete) ----
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -2,14 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config')
 
 const stuffCtrl = require('../controllers/stuff')
 
 // Pour protéger nos routes, on ajoute un middleware "auth" 
-router.get('/', auth, stuffCtrl.getAllStuff)
+router.get('/', auth, multer, stuffCtrl.getAllStuff)
 router.post('/', auth, stuffCtrl.createThing)
 router.get('/:id', auth, stuffCtrl.getOneThing)
 router.put('/:id', auth, stuffCtrl.modifyThing)
 router.delete('/:id', auth, stuffCtrl.deleteThing)
 
-module.exports = router;
+module.exports = router
